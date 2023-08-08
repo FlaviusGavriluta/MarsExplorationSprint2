ac53ef088d9331d78c0ffc4f3bb4562baca467ac
Daniel <dani.marinescu7@gmail.com>
2023-08-08 14:28:14 +0300

    Refactored MapLoaderImpl

--- src/main/java/com/codecool/marsexploration/mapexplorer/maploader/MapLoaderImpl.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/maploader/MapLoaderImpl.java
@@ -5,30 +5,29 @@ import com.codecool.marsexploration.mapexplorer.maploader.model.Map;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapLoaderImpl implements MapLoader{
     @Override
     public Map load(String mapFile) {
-        String[][] representation = new String[mapFile.length()][];
-        try {
-            Path path = Paths.get(mapFile);
-            BufferedReader reader = Files.newBufferedReader(path);
+        List<String[]> representationList = new ArrayList<>();
 
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(mapFile))) {
             String line;
-            int count = 0;
             while ((line = reader.readLine()) != null) {
-                representation[count] = line.split("");
-                count++;
+                representationList.add(line.split(""));
             }
-
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(Arrays.deepToString(representation));
-        return null;
+
+        String[][] representation = new String[representationList.size()][];
+        for (int i = 0; i < representationList.size(); i++) {
+            representation[i] = representationList.get(i);
+        }
+
+        return new Map(representation, true);
     }
 }

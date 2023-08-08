65333bea7588f722f9dfd39465721c92902cfe62
Daniel <dani.marinescu7@gmail.com>
2023-08-08 14:36:22 +0300

    The MapLoader implementation is covered with unit tests

---
+++ src/test/java/com/codecool/marsexploration/mapexplorer/maploader/MapLoaderImplTest.java
@@ -0,0 +1,19 @@
+package com.codecool.marsexploration.mapexplorer.maploader;
+
+import com.codecool.marsexploration.mapexplorer.maploader.model.Map;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MapLoaderImplTest {
+    private static final String workDir = "src/main";
+
+    @Test
+    void testLoad() {
+        MapLoader mapLoader = new MapLoaderImpl();
+        Map loadedMap = mapLoader.load(workDir + "/resources/exploration-0.map");
+
+        assertNotNull(loadedMap);
+    }
+}

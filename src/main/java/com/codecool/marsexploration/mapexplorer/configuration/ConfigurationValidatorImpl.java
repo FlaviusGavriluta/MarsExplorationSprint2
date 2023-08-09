3901e56c002b760973f7292e1c9bec83fb2e824e
ChircuAndrei <chircu.andrei93@gmail.com>
2023-08-09 22:29:43 +0300

    Refactor ConfigurationValidatorImpl

--- src/main/java/com/codecool/marsexploration/mapexplorer/configuration/ConfigurationValidatorImpl.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/configuration/ConfigurationValidatorImpl.java
@@ -7,74 +7,43 @@ import com.codecool.marsexploration.mapexplorer.maploader.model.Map;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class ConfigurationValidatorImpl implements ConfigurationValidator {
-    private MapLoader mapLoader = new MapLoaderImpl();
+    private final MapLoader mapLoader = new MapLoaderImpl();
 
     @Override
     public boolean validateConfiguration(Configuration configuration) {
         Map map = mapLoader.load(configuration.path());
-        if (map == null) {
+        if (map == null || configuration.symbols().isEmpty() || configuration.steps() <= 0) {
             return false;
         }
-        if(configuration.symbols().isEmpty()) {
-            return false;
-        }
-        if(configuration.steps() <= 0) {
-            return false;
-        }
+
         int dimension = map.getDimension();
         if (!map.isEmpty(configuration.coordinate())) {
             return false;
         }
 
         List<Coordinate> adjacentCoords = getAdjacentCoordinates(configuration.coordinate(), dimension);
-        return adjacentCoords.stream()
-                .filter(coordinate -> map.isEmpty(coordinate)).count() == 0;
-
+        return adjacentCoords.stream().noneMatch(coordinate -> !map.isEmpty(coordinate));
     }
 
-    public List<Coordinate> getAdjacentCoordinates(Coordinate coordinate, int dimension) {
+    private List<Coordinate> getAdjacentCoordinates(Coordinate coordinate, int dimension) {
         List<Coordinate> adjacentCoordinates = new ArrayList<>();
 
         int x = coordinate.X();
         int y = coordinate.Y();
 
-        // Top
-        if (x - 1 >= 0) {
-            adjacentCoordinates.add(new Coordinate(x - 1, y));
-        }
-        // Bottom
-        if (x + 1 < dimension) {
-            adjacentCoordinates.add(new Coordinate(x + 1, y));
-        }
-        // Left
-        if (y - 1 >= 0) {
-            adjacentCoordinates.add(new Coordinate(x, y - 1));
-        }
-        // Right
-        if (y + 1 < dimension) {
-            adjacentCoordinates.add(new Coordinate(x, y + 1));
-        }
-        // Top Left
-        if (x - 1 >= 0 && y - 1 >= 0) {
-            adjacentCoordinates.add(new Coordinate(x - 1, y - 1));
-        }
-        // Top Right
-        if (x - 1 >= 0 && y + 1 < dimension) {
-            adjacentCoordinates.add(new Coordinate(x - 1, y + 1));
-        }
-        // Bottom Left
-        if (x + 1 < dimension && y - 1 >= 0) {
-            adjacentCoordinates.add(new Coordinate(x + 1, y - 1));
-        }
-        // Bottom Right
-        if (x + 1 < dimension && y + 1 < dimension) {
-            adjacentCoordinates.add(new Coordinate(x + 1, y + 1));
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if (dx == 0 && dy == 0) continue;  // Skip the center coordinate
+                int newX = x + dx;
+                int newY = y + dy;
+                if (newX >= 0 && newX < dimension && newY >= 0 && newY < dimension) {
+                    adjacentCoordinates.add(new Coordinate(newX, newY));
+                }
+            }
         }
 
         return adjacentCoordinates;
     }
-
 }

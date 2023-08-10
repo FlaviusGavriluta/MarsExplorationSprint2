a9405726fa9a047ed7e4e801a6ceecca281db50e
Daniel <dani.marinescu7@gmail.com>
2023-08-10 11:30:22 +0300

    Implemented FileLogger

--- src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
@@ -1,5 +1,7 @@
 package com.codecool.marsexploration.mapexplorer;
 
+import com.codecool.marsexploration.mapexplorer.logger.FileLogger;
+import com.codecool.marsexploration.mapexplorer.logger.Logger;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoader;
 import com.codecool.marsexploration.mapexplorer.maploader.MapLoaderImpl;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
@@ -9,9 +11,12 @@ public class Application {
 
     public static void main(String[] args) {
         String mapFile = workDir + "/resources/exploration-0.map";
+        String logPath = workDir + "/resources/log/log.txt";
         Coordinate landingSpot = new Coordinate(6, 6);
         MapLoader mapLoader = new MapLoaderImpl();
         mapLoader.load(mapFile);
+        Logger fileLogger = new FileLogger(logPath);
+        fileLogger.log("This");
         // Add your code here
     }
 }
---
+++ src/main/java/com/codecool/marsexploration/mapexplorer/logger/FileLogger.java
@@ -0,0 +1,22 @@
+package com.codecool.marsexploration.mapexplorer.logger;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FileLogger implements Logger{
+    public final String filePath;
+
+    public FileLogger(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public void log(String message) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath, true))) {
+            writer.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

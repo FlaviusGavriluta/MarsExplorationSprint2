257dba999180dd22bc7bf40efd8395633177bb2a
Daniel <dani.marinescu7@gmail.com>
2023-08-10 12:24:02 +0300

    Simulation getters and setters

--- src/main/java/com/codecool/marsexploration/mapexplorer/simulation/Simulation.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/simulation/Simulation.java
@@ -16,4 +16,69 @@ public class Simulation {
     public List<String> symbols;
     public ExplorationOutcome outcome;
 
+    public Simulation(int steps, int stepsTimeout, Rover rover, Coordinate spaceshipLocation, Map map, List<String> symbols, ExplorationOutcome outcome) {
+        this.steps = steps;
+        this.stepsTimeout = stepsTimeout;
+        this.rover = rover;
+        this.spaceshipLocation = spaceshipLocation;
+        this.map = map;
+        this.symbols = symbols;
+        this.outcome = outcome;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public int getStepsTimeout() {
+        return stepsTimeout;
+    }
+
+    public void setStepsTimeout(int stepsTimeout) {
+        this.stepsTimeout = stepsTimeout;
+    }
+
+    public Rover getRover() {
+        return rover;
+    }
+
+    public void setRover(Rover rover) {
+        this.rover = rover;
+    }
+
+    public Coordinate getSpaceshipLocation() {
+        return spaceshipLocation;
+    }
+
+    public void setSpaceshipLocation(Coordinate spaceshipLocation) {
+        this.spaceshipLocation = spaceshipLocation;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public List<String> getSymbols() {
+        return symbols;
+    }
+
+    public void setSymbols(List<String> symbols) {
+        this.symbols = symbols;
+    }
+
+    public ExplorationOutcome getOutcome() {
+        return outcome;
+    }
+
+    public void setOutcome(ExplorationOutcome outcome) {
+        this.outcome = outcome;
+    }
 }
